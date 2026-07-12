07f0bb036db0eff51cfe6fe004aaeb0201339de1
MediaForge-org <maximwillinger111@gmail.com>
2026-07-12 02:45:35 +0200

    test: disable Vite manifest requirement in smoke test

--- tests/Feature/SmokeTest.php
+++ tests/Feature/SmokeTest.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 test('the welcome page renders', function () {
+    $this->withoutVite();
+
     $this->get('/')
         ->assertOk()
         ->assertSee('MediaForge');
 });
 
 test('the health endpoint responds', function () {
-    $this->get('/up')->assertOk();
+    $this->get('/up')
+        ->assertOk();
 });
